42474b1cf58bda27737f17502e4e3551ee1e3095
samidha <samidhawani7@gmail.com>
2026-05-28 13:13:35 +0530

    Update home route message in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask is working!"
+    return "Hello, Flask is working from samidha wani!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
